f8933b763e26c99ff2c264f5f1894229ca761a10
Serhat Demir <Serhatdmrw@gmail.com>
2023-03-15 23:07:56 +0300

    [feat] Create project and add login page.

---
+++ SemsNewsProject/Screens/Login/LoginViewController.swift
@@ -0,0 +1,83 @@
+//
+//  LoginViewController.swift
+//  SemsNewsProject
+//
+//  Created by Serhat Demir on 14.03.2023.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Outles
+    @IBOutlet private weak var emailText: UITextField!
+    @IBOutlet private weak var passwordText: UITextField!
+    
+    // MARK: - Properties
+    private let viewModel = LoginViewModel()
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addDelegates()
+    }
+    
+    // MARK: - Actions
+    @IBAction func didTapSignInButton(_ sender: Any) {
+        
+        guard let email = emailText.text, let password = passwordText.text else {
+            makeAlert(tittleInput: "ERROR", messegaInput: "Please check your login information.")
+            return
+        }
+        
+        viewModel.signIn(email: email, password: password)
+    }
+    
+    @IBAction func didTapSignUpButton(_ sender: Any) {
+      
+        guard let email = emailText.text, let password = passwordText.text else {
+            makeAlert(tittleInput: "Error", messegaInput: "Please enter a valid email or password.")
+            return
+        }
+        
+        viewModel.createUser(email: email, password: password)
+    }
+}
+
+// MARK: - Helpers
+private extension LoginViewController {
+    
+    func makeAlert(tittleInput: String, messegaInput: String) {
+        let alert = UIAlertController(title: tittleInput, message: messegaInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+    
+    func addDelegates() {
+        viewModel.delegate = self
+    }
+}
+
+// MARK: - LoginViewModelDelegate
+extension LoginViewController: LoginViewModelDelegate {
+    
+    func didSignInSuccess() {
+        // Bir sonraki sayfaya geçiş yapılmalı.
+        print("didSignInSuccess")
+    }
+    
+    func didSignInFail(message: String) {
+        makeAlert(tittleInput: "Error", messegaInput: message)
+    }
+    
+    func didCreateSuccess() {
+        makeAlert(tittleInput: "Success", messegaInput: "Congratulations, your registration has been successfully done. You can login.")
+    }
+    
+    func didCreateFail(message: String) {
+        makeAlert(tittleInput: "Error", messegaInput: message)
+    }
+}
---
+++ SemsNewsProject/Screens/Login/LoginViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewModel.swift
+//  SemsNewsProject
+//
+//  Created by Emre Büyüker on 15.03.2023.
+//
+
+import Foundation
+import FirebaseAuth
+
+protocol LoginViewModelDelegate: AnyObject {
+    func didSignInSuccess()
+    func didSignInFail(message: String)
+    func didCreateSuccess()
+    func didCreateFail(message: String)
+}
+
+class LoginViewModel {
+    
+    // MARK: - Delegate
+    weak var delegate: LoginViewModelDelegate?
+    
+    func signIn(email: String, password: String) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            
+            if let error = error {
+                self.delegate?.didSignInFail(message: error.localizedDescription)
+                return
+            }
+            
+            self.delegate?.didSignInSuccess()
+        }
+    }
+    
+    func createUser(email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            
+            if let error = error {
+                self.delegate?.didCreateFail(message: error.localizedDescription)
+                return
+            }
+            
+            self.delegate?.didCreateSuccess()
+        }
+    }
+}
