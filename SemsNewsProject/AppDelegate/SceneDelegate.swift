4c0a242fae1e329085e26196eb2c561ff8c03941
Serhat Demir <Serhatdmrw@gmail.com>
2023-03-18 00:43:56 +0300

    [feet] Add login flow.

--- SemsNewsProject/AppDelegate/SceneDelegate.swift
+++ SemsNewsProject/AppDelegate/SceneDelegate.swift
@@ -6,16 +6,21 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        let currentUser = Auth.auth().currentUser
+        if currentUser != nil {
+            let navigationController = UINavigationController(rootViewController: HomeViewController())
+            self.window?.rootViewController = navigationController
+            self.window?.makeKeyAndVisible()
+        }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
